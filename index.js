ae655b1cd998592004d4fc31053a5b300d510646
Emmanuel Sarpong <esarpong51@gmail.com>
2022-04-22 07:36:04 +0000

    balance check test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Web3 = require("web3")
 const fs = require("fs");
 
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_HTTP_PROVIDER));
+const account = web3.eth.accounts.wallet.add(process.env.ETH_ACCOUNT_PRIVATE_KEY)
 
 const loadSol = (sol = "Main", contract = "Main") => {
     const bytecode = fs.readFileSync(`bin/${sol}_sol_${contract}.bin`, {encoding: 'utf-8'})
@@ -12,18 +13,15 @@ const loadSol = (sol = "Main", contract = "Main") => {
 }
 
 const deploy = async ({bytecode, abi}) => {
-    const MainContract = await (new web3.eth.Contract(abi))
-        .deploy({data: bytecode})
-        .send({
-            from: process.env.ETH_ACCOUNT_ADDRESS,
-            gas: +process.env.ETH_TXN_GAS,
-            gasPrice: web3.utils.toWei('0.00003', 'ether')
-        });
-
-    return MainContract;
+    return new web3.eth.Contract(abi, null, {
+        from: account.address,
+        gas: +process.env.ETH_TXN_GAS,
+        gasPrice: web3.utils.toWei(process.env.ETH_TXN_GAS_PRICE, 'ether'),
+        data: bytecode
+    }).deploy().send();
 }
 
-deploy(loadSol()).then(contract => {
-    console.log(contract.methods.balance())
-})
+deploy(loadSol()).then(contract =>
+    contract.methods.balance().call()
+).then(console.log)
 
